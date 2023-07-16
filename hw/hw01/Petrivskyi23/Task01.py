bb9cb3bb60fa365bf0e4823e04becd536cfb68e1
Petrivskyi23 <vpetrivskij2303@gmail.com>
2023-07-16 18:15:35 +0300

    update Task01 and Task02 in hw01

---
+++ hw/hw01/Petrivskyi23/Task01.py
@@ -0,0 +1,7 @@
+user_name = input("What is your name?\n")
+user_age = input("How old are you?\n")
+user_live = input("Wher do you live?\n")
+
+print(f"Hello, {user_name} !!! ")
+print(f"Your age is {user_age} years")
+print(f"You live in {user_live}")
