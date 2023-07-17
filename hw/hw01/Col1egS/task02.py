b842efb40704ea31225a32f2fa02d6d79e9313a9
Oleh Ivanytskyi <ivanytskyi02oleh@gmail.com>
2023-07-18 02:02:17 +0300

    new file:   hw/hw01/Col1egS/__init__.py 	new file:   hw/hw01/Col1egS/task01.py 	new file:   hw/hw01/Col1egS/task02.py 	new file:   hw/hw02/Col1egS/HW1.PNG 	new file:   hw/hw02/Col1egS/HW11.PNG 	new file:   hw/hw02/Col1egS/__init__.py

---
+++ hw/hw01/Col1egS/task02.py
@@ -0,0 +1,18 @@
+a = float(input("A="))
+b = float(input("B="))
+
+sum = a + b
+difference = a - b
+product = a * b
+division = a / b
+exponentiation = a ** b
+floor = a // b
+modulo = a % b
+
+print("A + B =", sum)
+print("A - B =", difference)
+print("A * B =", product)
+print("A / B =", division)
+print("A ** B =", exponentiation)
+print("A // B =", floor)
+print("A % B =", modulo)
