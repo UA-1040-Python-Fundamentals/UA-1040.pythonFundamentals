d092b2f30e3b522920a368edebccad22ec477b3a
Nataliia Kostyk <natakostyk33@gmail.com>
2023-07-23 22:42:49 +0400

    KostykHW3

---
+++ hw/hw03/NataliiaKostyk/Task2.py
@@ -0,0 +1,12 @@
+number = (input("Input four-digit natural number: "))
+number_str = str(number)
+product = 1
+for digit in number_str:
+    product *= int(digit)
+print("Product of digits:", product)
+
+print("Reversed number:", number[::-1])
+
+digits = list(number_str)
+sorted_digits = sorted(digits)
+print("Sorted number:", (''.join(sorted_digits)))
---
+++ hw/hw03/NataliiaKostyk/Task3.py
@@ -0,0 +1,7 @@
+my_set = {1, 2, 3, 4, 5}
+my_list = ['apple', 'mango', 'orange', 35]
+
+my_set, my_list = my_list, my_set
+
+print('My set is ', my_set)
+print('My list is ', my_list)
