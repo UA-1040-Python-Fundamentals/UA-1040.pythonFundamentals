06260792502fb12f9a850a0307afe412cc8aad1a
Liubomyr Halamaha <galamagal86@gmail.com>
2023-08-04 19:06:44 +0300

    added lesson05

---
+++ lessons/lesson05/lesson05.py
@@ -0,0 +1,189 @@
+# i = 0
+# while i < 5:
+#     print(i, end=" -> ")
+#     print(i**i)
+#     i += 1
+# print("end")
+#
+# while i > 0:
+#     print(i, end=" -> ")
+#     print(i**i)
+#     i -= 1.5
+# print("end")
+import random
+
+#
+# for number in "test":
+#     print(number)
+# print("end")
+#
+# for element in (1, 2, 3, "4", (1, 2, 3), 5.4):
+#     print(element, type(element))
+# print("end")
+#
+# for element in 25:
+#     print(element, type(element))
+# print("end")
+
+# print(list(range(5)))
+# print(list(range(-5)))
+# print(list(range(5, 9)))
+# print(list(range(-5, 9)))
+# print(list(range(-5, 9, 3)))
+
+l = (1, 2, 3, "4", (1, 2, 3), 5.4)
+# for i in range(len(l)):
+#     print(f"l[{i}]={l[i]}")
+# a, b = (1, 2)
+# c, d, e = [8, 7, 6]
+# print(a, b, c, d, e)
+# print(list(enumerate(l)))
+# for element in enumerate(l):
+#     print(element, element[0], element[1])
+# for index, value in enumerate(l):
+#     print(index, value)
+l = []
+# for i in range(10):
+#     value = input(f"enter {i} element: ")
+#     l.append(value)
+# print(l)
+# for i in range(10):
+#     value = int(input(f"enter {i} element: "))
+#     if value > 0:
+#         l.append(value)
+# print(l)
+# i = 0
+# while len(l) < 4:
+#     value = int(input(f"enter {i+1} element: "))
+#     if value > 0:
+#         l.append(value)
+#         i += 1
+# print(l)
+# for _ in range(3):
+#     i = 0
+#     l = []
+#     while len(l) < 10:
+#         value = random.randint(-10, 10)
+#         if value > 0:
+#             l.append(value)
+#             i += 1
+#         else:
+#             print(value, end=" ")
+#     print(l)
+
+
+# while True:
+#     value = int(input(f"enter element: "))
+#     if value == 0:
+#         break
+#     print("my value is: ", value)
+
+# while True:
+#     value = int(input(f"enter element: "))
+#     if value < 0:
+#         print("value is negative")
+#         continue
+#     print(value)
+
+# for i in range(10):
+#     if i % 2:
+#         continue
+#     print(i)
+# print("test1", "test2", "test3")
+# print("test1", "test2", "test3", sep="_*_")
+# print("test1", "test2", "test3", sep="-", end="&&&")
+# print("test1", "test2", "test3", sep="_*_", end="&&&")
+# print("END")
+# print("END")
+
+# f = open("test.txt", "w")
+# print("test1", "test2", "test3", file=f)
+# print("test1", "test2", "test3", sep="_*_")
+# print("test1", "test2", "test3", sep="-", end="&&&", file=f)
+# print("test1", "test2", "test3", sep="_*_", end="&&&")
+# print("END", file=f)
+# print("END")
+
+# for i in range(3):
+#     print(i, end="\t")
+# else:
+#     print("end")
+#
+# print('END')
+# for i in range(3):
+#     print(i, end="\t")
+#     continue
+#
+# else:
+#     print("end")
+#
+# print('END')
+#
+# for i in range(3):
+#     print(i, end="\t")
+#     if i == 2:
+#         break
+# else:
+#     print("end")
+#
+# print('END')
+
+# for i in range(3):
+#     print(i, end="\t")
+#
+# else:
+#     print("end")
+# l = []
+# for i in range(10):
+#     temp_list = []
+#     for j in range(10):
+#         temp_list.append(random.randint(-1, 10))
+#     l.append(temp_list)
+# for i in l: print(i)
+# print("=" * 10)
+# for row in l:
+#     count = 0
+#     for element in row:
+#         if element < 0:
+#             count += 1
+#     if count == 0:
+#         print(row)
+# print("=" * 10)
+# for row in l:
+#     for element in row:
+#         test_my = "test"
+#         if element < 0:
+#             break
+#     else:
+#         print(row)
+#
+# print(row)
+# print(test_my)
+# l=0
+# for i in range(10):
+#     pass
+# for i in range(10):
+#     pass
+# for i in range(10):
+#     pass
+
+
+# def binary(decimal):
+#     code ...
+#     return result
+# decimal = input("text")#decimal = jdsifbvsdjhbfks
+# # code ...
+# print(result)
+#
+# void binary(decimal):
+# #     code ...
+# #     return result
+# function binary(decimal){}
+# binary = function (decimal){}
+
+
+
+
+
+
+
