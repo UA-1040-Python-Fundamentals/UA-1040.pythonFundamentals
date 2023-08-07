cabcbfdc4bc3caa1d950c0e5e6653f25a2f11ce8
Oleh Nadvirnianskyi <olehnadvirnianskyi@gmail.com>
2023-08-07 20:01:56 +0300

    Homework4

--- hw/hw04/OlehNadvirnianskyi/Task1.py
+++ hw/hw04/OlehNadvirnianskyi/Task1.py
@@ -1,8 +1,8 @@
 celsius = float(input("Enter the temperature in Celsius:"))
 fahrenheit = int((celsius * 9/5) + 32)
-if celsius == -273.15 or celsius < -273.15:
+if celsius == -273.15 or celsius < -273.15 :
     print("Temperature below absolute zero (-273.15°C)")
-elif celsius in range(-273,15):
+elif celsius in range(-273,0):
     print(f"{celsius}°C is equivalent to {fahrenheit}°F")
 else:
     print(f"{celsius}°C is equivalent to {fahrenheit}°F")
