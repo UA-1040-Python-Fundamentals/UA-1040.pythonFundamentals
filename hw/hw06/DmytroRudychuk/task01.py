bfaeaf9d6cd32a0ff7adf1470d3fcbac7c7384c7
RudychukDmytro <dmytrorudychuk@gmail.com>
2023-08-09 22:29:47 +0300

    DmytroRudychuk_hw06

---
+++ hw/hw06/DmytroRudychuk/task01.py
@@ -0,0 +1,13 @@
+
+
+even_numbers = [ x for x in range(1, 11) if x % 2 == 0]
+print(even_numbers)
+odd_num = [x for x in range(1, 11) if x % 3 ==0 and x % 2 !=0 ]
+print(odd_num)
+num = [x for x in range(1, 11) if x % 3 != 0 and x % 2 !=0 ]
+print(num)
+
+
+
+
+
