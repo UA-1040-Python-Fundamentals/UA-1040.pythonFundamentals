de26f83affc5a658ee15f1449b93541555a3e104
Nataliia Kostyk <117041158+NataliiaKostyk@users.noreply.github.com>
2023-08-09 23:51:13 +0400

    Update Task2.py

--- hw/hw06/NataliiaKostyk/Task2.py
+++ hw/hw06/NataliiaKostyk/Task2.py
@@ -3,7 +3,7 @@ login = "First"
 while True:
     user_input = input("Enter your login: ")
     if user_input == login:
-        print(f"Hello, ", login + "!")
+        print(f"Hello, {login}!")
         break
     else:
         print("Invalid login. Please try again.")
