e9b42947c2310739f43f98cb4455ecc5cb1b0095
Michailo <wonderwork025@gmail.com>
2023-08-11 13:36:36 +0300

    delete

--- hw/hw05/MischaPerehinchuk/HW5.py
+++ hw/hw05/MischaPerehinchuk/HW5.py
@@ -10,5 +10,22 @@ for i in my_list:
 print("end")
 
 #HW-05.2
-fibonaci_num
+
+number = int(input("Enter the number to which you want to hit the Fibonacci number: "))
+if number <= 0:
+    print("Please enter a number greater than 0")
+else:
+    number1 = 0
+    number2 = 1
+
+    for i in range(number):
+        if number1 > number:
+            break
+        else:
+            print(number1)
+            number1,number2 = number2, number1+number2
+print("End")
+
+
+# HW-05.3
 
