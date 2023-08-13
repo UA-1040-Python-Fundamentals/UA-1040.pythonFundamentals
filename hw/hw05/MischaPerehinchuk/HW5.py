e4727871de4e267c4b6c40f7c9097848c288ca2b
Michailo <wonderwork025@gmail.com>
2023-08-13 14:20:16 +0300

    HW5

--- hw/hw05/MischaPerehinchuk/HW5.py
+++ hw/hw05/MischaPerehinchuk/HW5.py
@@ -29,4 +29,12 @@ print("End")
 
 # HW-05.3
 
+n = int(input("Enter the number: "))
+if n < 0:
+    print("Factorial 0 is not defined")
+else:
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
 
+    print(f"{n}! = {factorial}")
