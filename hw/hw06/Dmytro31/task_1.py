3a205f684fdd169d6179e4049a2d51377e00d567
Dmytro31 <dima321fedoruk@gmail.com>
2023-08-13 14:41:12 +0300

    load

---
+++ hw/hw06/Dmytro31/task_1.py
@@ -0,0 +1,17 @@
+
+even_divisible_by_2 = []
+odd_divisible_by_3 = []
+not_divisible_by_2_and_3 = []
+
+for num in range(1, 11):
+    if num % 2 == 0:
+        even_divisible_by_2.append(num)
+    if num % 2 == 1 and num % 3 == 0:
+        odd_divisible_by_3.append(num)
+    if num % 2 != 0 and num % 3 != 0:
+        not_divisible_by_2_and_3.append(num)
+
+
+print("Even numbers divisible by 2:", even_divisible_by_2)
+print("Odd numbers divisible by 3:", odd_divisible_by_3)
+print("Numbers not divisible by 2 and 3:", not_divisible_by_2_and_3)
