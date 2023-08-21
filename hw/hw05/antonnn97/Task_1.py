fb2c95add1ec80c668a580d7398a0786e518bfe4
antondev97 <anton.savka.developer@gmail.com>
2023-08-21 22:27:05 +0300

    added three tasks

---
+++ hw/hw05/antonnn97/Task_1.py
@@ -0,0 +1,6 @@
+int_list = [65, 33, 9011, 54, 56, 10, 19, 29, 31, 10000000]
+
+float_list = [float(element) for element in int_list]
+
+print("Integer list:", int_list)
+print("Float list:", float_list)
