306a72ebd5999f4ff58a0975a46c25b626d07a5b
Liubomyr Halamaha <galamagal86@gmail.com>
2023-08-22 19:02:06 +0300

    added lesson08.2

--- lessons/lesson08/lesson08.py
+++ lessons/lesson08/lesson08.py
@@ -8,8 +8,8 @@
 # print(getcwd())
 # import os
 # print(os.getcwd())
-def to_json():
-    #todo
+# def to_json():
+#     #todo
 # import sys
 # import pprint
 # sys.path.append("c:\\data")
@@ -27,10 +27,29 @@ def to_json():
 # print(COs(0.5))
 # print(tan(0.5))
 # import copy_lesson
-from copy_lesson import pi
-print("lesson08.py", __name__)
-# from lessons.lesson04.lesson04 import a
-import lessons.lesson04.lesson04 as l4
-print("a: ", l4.a)
-if __name__ == "__main__":
-    print("run lesson08.py")
+# from copy_lesson import pi
+# print("lesson08.py", __name__)
+# # from lessons.lesson04.lesson04 import a
+# import lessons.lesson04.lesson04 as l4
+# print("a: ", l4.a)
+# if __name__ == "__main__":
+#     print("run lesson08.py")
+# from pprint import pprint
+# import sys
+# pprint(sys.path)
+# print(dir())
+# from second import *
+# print(dir())
+# print(a)
+# # print(_a)
+# from second import _a, __a
+#
+# print(dir())
+
+# from pack.p1 import pp1
+from pack import pp1
+print(pp1)
+
+import pack2
+print(pp1)
+print(pack2)
