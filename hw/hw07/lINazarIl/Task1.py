d8206e3229ed87ab9765077764e43b79e83d0f9c
NazarGaliy <nazar.galiy@gmail.com>
2023-08-28 22:12:42 +0300

    Task 1 from hw7 completed

---
+++ hw/hw07/lINazarIl/Task1.py
@@ -0,0 +1,20 @@
+def largest_num(num1, num2):
+    return max([num1, num2])
+
+def main():
+    while True:
+        num1 = int(input('Please enter number 1: '))
+        num2 = int(input('Please enter number 2: '))
+        print('Largest number is: ', largest_num(num1, num2))
+
+        choice = input('Continue program Y/N:')
+        while not choice.lower() in ['y', 'n']:
+            choice = input('Error, correct choice is Y or N: ')
+
+        if choice.lower() == 'n':
+            break
+
+        print('\n\r'*3, end='')
+
+if __name__ == '__main__':
+    main()
