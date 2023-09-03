b2dba100e4952704f7770d9076385e04a3c1f3d8
Sviatoslav Brahin <holfamework@gmail.com>
2023-09-03 14:58:03 +0300

    degreeProject, SBrahin

---
+++ degree_projects/SBrahin/telegramBot/bot/kb.py
@@ -0,0 +1,15 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+menu = [
+    [InlineKeyboardButton(text="📝 Згенерувати текст", callback_data="generate_text"),
+    InlineKeyboardButton(text="🖼 Згенерувати зображення", callback_data="generate_image")],
+    [InlineKeyboardButton(text="💳 Зробити донат", callback_data="buy_tokens"),
+    InlineKeyboardButton(text="📲 Перейти до нашого каналу", callback_data="balance", url='https://t.me/Smartixxxxxx')],
+    #[InlineKeyboardButton(text="💎 Партнёрская программа", callback_data="ref"),
+    #InlineKeyboardButton(text="🎁 Бесплатные токены", callback_data="free_tokens")],
+    [InlineKeyboardButton(text="🔎 Допомога", callback_data="help")]
+]
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Вийти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Вийти в меню", callback_data="menu")]])
+
+
