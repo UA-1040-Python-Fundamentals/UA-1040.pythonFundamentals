efcefd13decb1a940aacb9ae4346ff2d2d9a06e2
Oleh Ivanytskyi <ivanytskyi02oleh@gmail.com>
2023-09-14 22:14:18 +0300

    hw07

--- hw/hw06/NataliiaKostyk/Task2.py
+++ hw/hw06/NataliiaKostyk/Task2.py
@@ -1,9 +1,4 @@
-login = "First"
-
-while True:
-    user_input = input("Enter your login: ")
-    if user_input == login:
-        print(f"Hello, {login}!")
-        break
-    else:
-        print("Invalid login. Please try again.")
+def q (x,y,z):
+    pass
+tuples = (1,2,3)
+q(tuples)
